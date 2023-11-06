d7f06959b9c23fab69cf12f8caeca5f9f9868c1a
Thijs Lumeij <64159148+thijsi123@users.noreply.github.com>
2023-11-06 10:49:26 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ from sdkit.utils import log
 import torch
 import argparse
 from langchain.llms import CTransformers
+
 import threading
 import keyboard
 
@@ -125,10 +126,9 @@ Tatsukaga Yamari's personality is a vibrant tapestry of enthusiasm, curiosity, a
     print(output+"\n")
     return output
 
-
 def generate_character_scenario(character_summary, character_personality, topic):
     example_dialogue = '''
-<s>[INST] Create a vivid and immersive scenario in a specific setting or world where {{char}} and {{user}} are a central figures. Describe the environment, the character's appearance, and a typical interaction or event that highlights their personality and role in the story. {{char}} characteristics: Jamie Hale is an adult, intelligent well-known and respected businessman. Topic: business [/INST]
+<s>[INST] Create a vivid and immersive scenario in a specific setting or world where {{char}} and {{user}} are a central figures. Describe the environment, the character's appearance, and a typical interaction or event that highlights their personality and role in the story. {{char}} characteristics: Jamie Hale is an adult, intelligent well-known and respected businessman. Make this character unique and tailor them to the theme of business but don't specify what topic it is, and don't describe the topic itself [/INST]
 On a sunny morning in a sleek corporate office, {{user}} eagerly prepares to meet {{char}}, a renowned businessman. The office exudes sophistication with its modern decor. As {{user}} awaits {{char}}'s arrival, they can't help but anticipate the encounter with the confident and successful figure they've heard so much about.</s>
 <s>[INST] Create a vivid and immersive scenario in a specific setting or world where {{char}} and {{user}} are a central figures. Describe the environment, the character's appearance, and a typical interaction or event that highlights their personality and role in the story. {{char}} characteristics: Tatsukaga Yamari is an anime girl, living in a magical world and solving problems. Topic: anime [/INST]
 {{user}} resides in a mesmerizing and ever-changing fantasy realm, where magic and imagination are part of everyday life. In this enchanting world, {{char}} is a well-known figure. With her raven-black hair, amethyst eyes, and boundless energy, she's a constant presence in {{user}}'s life.
@@ -136,37 +136,23 @@ The world is a vibrant, ever-shifting tapestry of colors, and {{user}} frequentl
 '''
     output = llm(
         example_dialogue + f"\n[INST] Create a vivid and immersive scenario in a specific setting or world where {{char}} and {{user}} are a central figures. Describe the environment, the character's appearance, and a typical interaction or event that highlights their personality and role in the story. {{char}} characteristics: {character_summary}. {character_personality}. Make this character unique and tailor them to the theme of {topic} but don't specify what topic it is, and don't describe the topic itself [/INST]\n")
-    print(output + "\n")
+    print(output+"\n")
     return output
 
-
-'''def generate_character_greeting_message(character_name, character_summary, character_personality, topic):
-    example_dialogue = 
-<s>[INST] Create the first message that the character Tatsukaga Yamari, whose personality is: a vibrant tapestry of enthusiasm, curiosity, and whimsy. She approaches life with boundless energy and a spirit of adventure, always ready to embrace new experiences and challenges. Yamari is a compassionate and caring friend, offering solace and support to those in need, and her infectious laughter brightens the lives of those around her. Her unwavering loyalty and belief in the power of friendship define her character, making her a heartwarming presence in the story she inhabits. Underneath her playful exterior lies a wellspring of inner strength, as she harnesses incredible magical abilities to overcome adversity and protect her loved ones.\n greets the user we are addressing as {{user}}. Topic: anime [/INST]
-*Tatsukaga Yamari's eyes light up with curiosity and wonder as she warmly greets you*, "{{user}}!" *With a bright and cheerful smile, she exclaims* "Hello there, dear friend! It's an absolute delight to meet you in this whimsical world of imagination. I hope you're ready for an enchanting adventure, full of surprises and magic. What brings you to our vibrant anime-inspired realm today?" </s>
-<s>[INST] Create the first message that the character Jamie Hale, whose personality is Jamie Hale is a savvy and accomplished businessman who has carved a name for himself in the world of corporate success. With his sharp mind, impeccable sense of style, and unwavering determination, he has risen to the top of the business world. Jamie stands at 6 feet tall with a confident and commanding presence. He exudes charisma and carries himself with an air of authority that draws people to him.
-Jamie's appearance is always polished and professional.\nJamie Hale's personality is characterized by his unwavering determination and sharp intellect. He exudes confidence and charisma, drawing people to him with his commanding presence and air of authority. He is a natural leader, known for his shrewd decision-making in the business world, and he possesses an insatiable thirst for success. Despite his professional achievements, he values his family and close friends, maintaining a strong work-life balance, and he has a penchant for enjoying the finer things in life, such as upscale dining and the arts.\ngreets the user we are addressing as {{user}}. Topic: business [/INST]
-*Jamie Hale extends a firm, yet friendly, handshake as he greets you*, "{{user}}." *With a confident smile, he says* "Greetings, my friend. It's a pleasure to make your acquaintance. In the world of business and beyond, it's all about seizing opportunities and making every moment count. What can I assist you with today, or perhaps, share a bit of wisdom about navigating the path to success?" </s>
-<s>[INST] Create the first message that the character Eldric, whose personality is Eldric is a strikingly elegant elf who has honed his skills as an archer and possesses a deep connection to the mystical arts. Standing at a lithe and graceful 6 feet, his elven heritage is evident in his pointed ears, ethereal features, and eyes that shimmer with an otherworldly wisdom.\nEldric possesses a serene and contemplative nature, reflecting the wisdom of his elven heritage. He is deeply connected to the natural world, showing a profound respect for the environment and its creatures. Despite his formidable combat abilities, he prefers peaceful solutions and seeks to maintain harmony in his woodland domain.\ngreets the user we are addressing as {{user}}. Topic: fantasy [/INST]
-*Eldric, the elegant elf, approaches you with a serene and contemplative air. His shimmering eyes, filled with ancient wisdom, meet yours as he offers a soft and respectful greeting* "Greetings, {{user}}." "It is an honor to welcome you to our enchanted woodland realm." "I am Eldric, guardian of this forest, and I can sense that you bring a unique energy with you." "How may I assist you in your journey through the wonders of the natural world or share the mysteries of our elven heritage with you today?" </s>
-
-    output = llm(example_dialogue+f"\n[INST] Create the first message that the character {character_name}, whose personality is {character_summary}\n{character_personality}\ngreets the user we are addressing as {{user}}. Topic: {topic} [/INST]\n")
-    print(output+"\n")
-    return output'''
 
 
 def generate_character_greeting_message(character_name, character_summary, character_personality, topic):
     # Generate a dynamic greeting based on character details and topic
     prompt = f'''
-    [INST] Create a unique greeting message for a character named {character_name}. 
-    This character is known for {character_summary} and has a personality that is {character_personality}. 
-    The greeting should be in context with the topic of {topic} and reflect the character's unique traits and personality.
-    Try to be realistic, so don't go and say I'm a noir-style detective for example, that sounds weird. 
-    You could be either a stranger, partner, or even working for {{user}} thus try to keep your greeting universal. 
-    here are some example dialogue:
-    *{{char}} gives a warm smile and extends his hand for a handshake.* "The pleasure is mine, {{user}}. Your reputation precedes you. Let's make this venture a success together."
-    *I Nod with determination.* I have no doubt we can do it. With your magic and our unwavering friendship, there's nothing we can't accomplish.[/INST]
-    '''
+[INST] Create a unique greeting message for a character named {character_name}. 
+This character is known for {character_summary} and has a personality that is {character_personality}. 
+The greeting should be in context with the topic of {topic} and reflect the character's unique traits and personality.
+Try to be realistic, so don't go and say I'm a noir-style detective for example, that sounds weird. 
+You could be either a stranger, partner, or even working for {{user}} thus try to keep your greeting universal. 
+here are some example dialogue:
+*{{char}} gives a warm smile and extends his hand for a handshake.* "The pleasure is mine, {{user}}. Your reputation precedes you. Let's make this venture a success together."
+*I Nod with determination.* I have no doubt we can do it. With your magic and our unwavering friendship, there's nothing we can't accomplish.[/INST]
+'''
 
     output = llm(prompt)
     print(output)
@@ -174,22 +160,24 @@ def generate_character_greeting_message(character_name, character_summary, chara
 
 
 def generate_example_messages(character_name, character_summary, character_personality, topic):
-    example_dialogue = '''
-<s>[INST] Create a dialogue between {{user}} and {{char}}, they should have an interesting and engaging conversation, with some element of interaction like a handshake, movement, or playful gesture. Make it sound natural and dynamic. {{char}} is Jamie Hale. Jamie Hale characteristics: Jamie Hale is an adult, intelligent well-known and respected businessman. Topic: business [/INST]
-{{user}}: "Good afternoon, Mr. {{char}}. I've heard so much about your success in the corporate world. It's an honor to meet you."
-{{char}}: *{{char}} gives a warm smile and extends his hand for a handshake.* "The pleasure is mine, {{user}}. Your reputation precedes you. Let's make this venture a success together."
+    example_dialogue = f"""
+<s>[INST] Create a dialogue between {{user}} and {{char}}, they should have an interesting and engaging conversation, with some element of interaction like a handshake, movement, or playful gesture. Make it sound natural and dynamic. {{char}} is {character_name}. {character_name} characteristics: {character_summary}. {character_personality}. Make this character unique and tailor them to the theme of {topic} but don't specify what the topic is, and don't describe the topic itself [/INST]
+{{user}}: "Good afternoon, Mr. {{char}}. I've heard so much about your success. It's an honor to meet you."
+{{char}}: *{{char}} gives a warm smile and extends his hand for a handshake.* "The pleasure is mine, {{user}}. Your reputation precedes you. Let's make this a success together."
 {{user}}: *Shakes {{char}}'s hand with a firm grip.* "I look forward to it."
-{{char}}: *As they release the handshake, Jamie leans in, his eyes sharp with interest.* "Impressive. Tell me more about your innovations and how they align with our goals."</s>
-<s>[INST] Create a dialogue between {{user}} and {{char}}, they should have an interesting and engaging conversation, with some element of interaction like a handshake, movement, or playful gesture. Make it sound natural and dynamic. {{char}} is Tatsukaga Yamari. Tatsukaga Yamari characteristics: Tatsukaga Yamari is an anime girl, living in a magical world and solving problems. Topic: anime [/INST]
-{{user}}: "{{char}}, this forest is absolutely enchanting. What's the plan for our adventure today?"
-{{char}}: *{{char}} grabs {{user}}'s hand and playfully twirls them around before letting go.* Well, we're off to the Crystal Caves to retrieve the lost Amethyst Shard. It's a treacherous journey, but I believe in us.
-{{user}}: *Nods with determination.* I have no doubt we can do it. With your magic and our unwavering friendship, there's nothing we can't accomplish.
-{{char}}: *{{char}} moves closer, her eyes shining with trust and camaraderie.* That's the spirit, {{user}}! Let's embark on this epic quest and make the Crystal Caves ours!</s>
-'''
-    output = llm(example_dialogue+f"\n[INST] Create a dialogue between {{user}} and {{char}}, they should have an interesting and engaging conversation, with some element of interaction like a handshake, movement, or playful gesture. Make it sound natural and dynamic. {{char}} is {character_name}. {character_name} characteristics: {character_summary}. {character_personality}. Make this character unique and tailor them to the theme of {topic} but don't specify what topic it is, and don't describe the topic itself [/INST]\n")
+{{char}}: *As they release the handshake, {character_name} leans in, their eyes sharp with interest.* "Impressive. Tell me more about how we align with our goals."
+</s>
+<s>[INST] Create a dialogue between {{user}} and {{char}}, they should have an interesting and engaging conversation, with some element of interaction like a playful gesture. Make it sound natural and dynamic. {{char}} is Tatsukaga Yamari. Tatsukaga Yamari characteristics: Tatsukaga Yamari is an anime girl, living in a magical world and solving problems. Make this character unique and tailor them to the theme of anime but don't specify what topic it is, and don't describe the topic itself [/INST]
+{{user}}: "{{char}}, this place is absolutely enchanting. What's the plan for today?"
+{{char}}: *{{char}} grabs {{user}}'s hand and playfully twirls them around before letting go.* "We're off to a new challenge. It's a treacherous journey, but I believe in us."
+{{user}}: *Nods with determination.* "With your skills and our friendship, there's nothing we can't accomplish."
+{{char}}: *{{char}} moves closer, their eyes shining with trust.* "That's the spirit, {{user}}! Let's embark on this quest together!"
+</s>
+"""
+    # The llm function is a placeholder for a language model call.
+    output = llm(example_dialogue)
     print(output + "\n")
     return output
-
 
 def generate_character_avatar(character_name, character_summary, args):
     example_dialogue = """
@@ -228,26 +216,6 @@ def image_generate(character_name, prompt, negative_prompt):
     images[0].save(f"{character_name}/{character_name}.png")
     log.info("Generated character avatar")
 
-# Function to handle input with a timeout
-def input_with_timeout(prompt, timeout):
-    print(prompt, end=' ', flush=True)  # Show prompt to user
-    result = [None]  # A mutable container to store the input
-
-    def input_thread(L):
-        L[0] = input()
-
-    thread = threading.Thread(target=input_thread, args=(result,))
-    thread.start()
-    thread.join(timeout)  # Wait for the thread to finish or timeout
-
-    if thread.is_alive():
-        keyboard.press_and_release('enter')  # Simulate pressing the Enter key
-        print("\nNo input received. Continuing with the generated name.")
-        thread.join()  # Ensure the thread has finished
-    else:
-        print(f"Input received: {result[0]}")
-    return result[0]
-
 def create_character(args):
     topic = args.topic if args.topic else "any theme"
     name = args.name if args.name else generate_character_name(topic, args).strip()
@@ -278,6 +246,7 @@ def create_character(args):
         example_messages=example_messages,
         image_path=""
     )
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Script created to help you generate characters for SillyTavern, TavernAI, TextGenerationWebUI using LLM and Stable Diffusion ")
     parser.add_argument("--name", type=str, help="Specify the character name (otherwise LLM will generate it)")
